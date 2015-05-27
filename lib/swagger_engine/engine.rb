d059d37bd416737e9244dc656ef2775c0f5e7830
batdevis <batdevis@gmail.com>
2015-05-27 15:32:19 +0200

    assets precompile and favicon

--- lib/swagger_engine/engine.rb
+++ lib/swagger_engine/engine.rb
@@ -1,5 +1,10 @@
 module SwaggerEngine
   class Engine < ::Rails::Engine
     isolate_namespace SwaggerEngine
+
+    initializer "swagger_engine.assets.precompile" do |app|
+      app.config.assets.precompile += %w(print.css reset.css)
+    end
+
   end
 end
