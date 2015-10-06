aedaf633cff2b2cb4dbf3715de72ab24819270a6
batdevis <batdevis@gmail.com>
2015-10-06 13:40:03 +0200

    json file protected location

--- app/controllers/swagger_engine/swaggers_controller.rb
+++ app/controllers/swagger_engine/swaggers_controller.rb
@@ -11,12 +11,19 @@ module SwaggerEngine
     end
 
     def show
-      @swagger_json_url = @json_files[params[:id].to_sym]
+      respond_to do |format|
+        format.html { 
+          @swagger_json_url = swagger_path(params[:id], format: :json)
+        }
+        format.json { 
+          send_file @json_files[params[:id].to_sym], type: 'application/json', disposition: 'inline'
+        }
+      end
     end
 
     private
     def load_json_files
-      @json_files ||= SwaggerEngine.configuration.json_files || { default: "swagger.json" }
+      @json_files ||= SwaggerEngine.configuration.json_files || { default: "#{Rails.root}/lib/swagger_engine/swagger.json" }
     end
   end
 end
